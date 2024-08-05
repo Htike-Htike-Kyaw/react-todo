b57632e1937d13ae0941a92eb4855bf4488ed5b6
Htike-Htike-Kyaw <htikekyaw2020@gmail.com>
2024-08-05 21:43:48 +0630

    react-state-todo

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import Heading from './components/Heading'
+import CreateText from './components/CreateText'
+import TaskList from './components/TaskList'
+
+const App = () => {
+  const [tasks, setTask ] = useState([
+    { id: 3, task: 'a Lorem ipsum dolor sit amet.', isDone: true },
+    { id: 4, task: 'b Lorem ipsum dolor sit amet.', isDone: false },
+    { id: 5, task: 'c Lorem ipsum dolor sit amet.', isDone: true },
+    { id: 6, task: 'd Lorem ipsum dolor sit amet.', isDone: false },
+]);
+
+const addTask = (newTask)=>{
+    setTask([...tasks, newTask]);
+}
+
+const removeTask = (id)=>{
+  setTask(tasks.filter(task => task.id !== id));
+}
+
+const doneTask = (id)=>{
+  setTask(tasks.map(el => el.id === id ? { ...el, isDone: !el.isDone }: el));
+}
+
+  return (
+    <div className='p-10'>
+      <Heading />
+      <CreateText addTask={addTask} />
+      <TaskList doneTask={doneTask} removeTask={removeTask} tasks={tasks}  />
+    </div>
+  )
+}
+
+export default App
